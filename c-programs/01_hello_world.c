fb013be612c7d1d112759b3bab8509a48ecdfb38
Lydia S. Makiwa <makiwalydia35@gmail.com>
2026-04-08 21:17:09 +0530

    ⚙️ Add C: Hello World

---
+++ c-programs/01_hello_world.c
@@ -0,0 +1,10 @@
+/* C Program 1: Hello World
+   Author: Lydia S. Makiwa */
+
+#include <stdio.h>
+
+int main() {
+    printf("Hello, World!\n");
+    printf("Welcome to C Programming by Lydia S. Makiwa\n");
+    return 0;
+}
