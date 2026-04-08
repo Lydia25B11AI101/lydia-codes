12ac00b4db63478c455a5d3552024a1bfaad49a6
Lydia S. Makiwa <makiwalydia35@gmail.com>
2026-04-08 21:17:04 +0530

    🐍 Add Python: Hello World & greeting

---
+++ python/01_hello_world.py
@@ -0,0 +1,9 @@
+# Python Program 1: Hello World + User Greeting
+# Author: Lydia S. Makiwa
+
+def greet(name):
+    """Returns a personalised greeting."""
+    return f"Hello, {name}! Welcome to AIML programming 🤖"
+
+name = input("Enter your name: ")
+print(greet(name))
