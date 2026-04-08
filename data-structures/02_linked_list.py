150606a3c198bb764cb168cb5688092549b1cbeb
Lydia S. Makiwa <makiwalydia35@gmail.com>
2026-04-08 21:17:14 +0530

    📊 Add DSA: Singly linked list

---
+++ data-structures/02_linked_list.py
@@ -0,0 +1,50 @@
+# Data Structure 2: Singly Linked List
+# Author: Lydia S. Makiwa
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def append(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            return
+        current = self.head
+        while current.next:
+            current = current.next
+        current.next = new_node
+
+    def display(self):
+        elements = []
+        current = self.head
+        while current:
+            elements.append(str(current.data))
+            current = current.next
+        print(" → ".join(elements) + " → None")
+
+    def delete(self, data):
+        if not self.head:
+            return
+        if self.head.data == data:
+            self.head = self.head.next
+            return
+        current = self.head
+        while current.next:
+            if current.next.data == data:
+                current.next = current.next.next
+                return
+            current = current.next
+
+# Demo
+ll = LinkedList()
+for val in [10, 20, 30, 40, 50]:
+    ll.append(val)
+ll.display()
+ll.delete(30)
+ll.display()
